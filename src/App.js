7fa29b668c610e1d08ba1c39c3e9b8236d0cc5a5
Stalius4 <deivydasrimgaila@gmail.com>
2022-06-25 10:51:09 +0100

    before router

--- src/App.js
+++ src/App.js
@@ -7,29 +7,33 @@ import {Display, UserDisplay} from "./components/Display"
 import {UserList} from "./components/userList/userlist"
 import "./App.css"
 import  {delUser} from "./utils/utils"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+
+
+
+
 function App() {
-  const [user, setUser] = useState();
-  const [randomPic, setRandomPic] = useState([])
-  const [userList, setUserList] = useState([])
- const [jwt, setJwt] = useState (false)
- const[duplicateUser, setDuplicateUser] = useState(true)
- const[duplicateEmail, setDuplicateEmail] = useState(true)
 
-
-
+const [user, setUser] = useState();
+const [randomPic, setRandomPic] = useState([])
+const [userList, setUserList] = useState([])
+const [jwt, setJwt] = useState (false)
+const[duplicateUser, setDuplicateUser] = useState(true)
+const[duplicateEmail, setDuplicateEmail] = useState(true)
 
 
   useEffect (() => {
     fetchPhotos(setRandomPic)
     listUsers(setUserList)
- 
   }, [])
 
 
   return (
     <div className="App">
        <div>{jwt ? <Display randomPic={randomPic}/> : <Login user={user} setter={setUser} setJwt={setJwt} jwt={jwt} setDuplicateUser={setDuplicateUser} duplicateUser={duplicateUser} duplicateEmail={duplicateEmail} setDuplicateEmail={setDuplicateEmail}/>}</div>
-    <UserDisplay  user={user} userList={userList} setUserList={setUserList}/>
+    {/* <UserDisplay  user={user} userList={userList} setUserList={setUserList}/> */}
 {/* <UserList></UserList> */}
     
       {/* {randomPic.map((item, index) =>{
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,123 +2,151 @@ import insta from "./insta.png";
 import "./login.css";
 import { createUser, loginUser } from "../../utils/utils.js";
 import { useState } from "react";
-
-export const Login = ({ setter, setJwt, jwt, setDuplicateUser, user ,duplicateUser, setDuplicateEmail, duplicateEmail}) => {
+import { Link, Outlet } from 'react-router-dom';
+export const Login = ({
+  setter,
+  setJwt,
+  jwt,
+  setDuplicateUser,
+  user,
+  duplicateUser,
+  setDuplicateEmail,
+  duplicateEmail,
+}) => {
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-
   const [loginError, setLoginError] = useState(false);
   const [signUp, setSignUp] = useState(true);
-    const[emailUsernameCorrect, setEmailUsernameCorrect]= useState(false)
+  const [emailUsernameCorrect, setEmailUsernameCorrect] = useState(false);
 
   const submitHandler = (e) => {
-    
     e.preventDefault();
-    setDuplicateUser(true)
+    setDuplicateUser(true);
     //true - dont show
-    setDuplicateEmail(true)
-       //true - dont show
-    
-       //true - dont show
+    setDuplicateEmail(true);
+    //true - dont show
+
+    //true - dont show
     // console.log(emailUsernameCorrect, "sds")
-    console.log(user)
-    if(user=== undefined){  
-        setEmailUsernameCorrect(false)
-        //hide
-    }else if (user){
-        setEmailUsernameCorrect(true)
-        //show
+    console.log(user);
+    if (user === undefined) {
+      setEmailUsernameCorrect(false);
+      //hide
+    } else if (user) {
+      setEmailUsernameCorrect(true);
+      //show
     }
 
-    createUser(username, email, password, setter, setDuplicateUser, setDuplicateEmail);
-    
-    
+    createUser(
+      username,
+      email,
+      password,
+      setter,
+      setDuplicateUser,
+      setDuplicateEmail
+    );
   };
-
 
   const submitLogin = (e) => {
     e.preventDefault();
     loginUser(username, email, password, setJwt);
     console.log("jwt in login", jwt);
-    if (jwt === false ) {
+    if (jwt === false) {
       setLoginError(true);
-    } else {setLoginError(false)}
+    } else {
+      setLoginError(false);
+    }
   };
 
   return (
-   <>
-        {signUp ?    <div>  <div className="flex-container">
-      <img className="img" src={insta} alt="" />
-      <form className="flex-form" onSubmit={submitLogin}>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          className="form" required
-          type="text"
-          
-        /><span className="floating-label">Username</span>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          className="form"
-          type="password"
-          placeholder="Password"
-        />
-        <button className="button" type="submit">Login</button>
-      </form>
-      {loginError ? (<div className="wrong-pass"> Sorry, your password was incorrect. Please double-check your password. </div>) : ("")}
-    </div>
-    <div className="sign-up">
-  <p className="no-acc">Don't have an account? </p>
-  <span onClick={() => setSignUp(!signUp)} className="sign-up-text">         
-    Sign up
-  </span>
-  </div>
-  </div>
-   :      <div>  <div className="flex-container">
-          <img className="img" src={insta} alt=""  />
-          <p>Sign up to see photos and videos from your friends.</p>
-          <form className="flex-form" onSubmit={submitHandler}>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              className="form"
-              type="text"
-              placeholder="Username "
-            />
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              className="form"
-              type="text"
-              placeholder="Email"
-            />
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              className="form"
-              type="password"
-              placeholder="Password"
-            />
-            <button className="button" type="submit">Register</button>
-          </form>
-          {duplicateUser ? "": <div className="dub-message">Login already exists. </div> }
-    
-          {duplicateEmail ? "": <div className="dub-message">Email already exists. </div> }
-          {emailUsernameCorrect ?<div>User created</div>:""}
+    // Login container
+    <>
+      {signUp ? (
+        <div>
+          {" "}
+          <div className="flex-container">
+            <img className="img" src={insta} alt="" />
+            <form className="flex-form" onSubmit={submitLogin}>
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                className="form"  required type="text"/>
+              <span className="floating-label">Username</span>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className="form" required type="password"
+              />
+                <span className="floating-label">Password</span>
+              <button className="button" type="submit">
+                Login
+              </button>
+            </form>
+            {loginError ? (
+              <div className="wrong-pass">
+                {" "}
+                Sorry, your password was incorrect. Please double-check your
+                password.{" "}
+              </div>
+            ) : (
+              ""
+            )}
           </div>
           <div className="sign-up">
-  <p className="no-acc">Have an account?  </p>
-  <span onClick={() => setSignUp(!signUp)} className="sign-up-text">         
-    Log in
-  </span>
-  </div>
-         
-          </div> }
+            <p className="no-acc">Don't have an account? </p>
+            <span onClick={() => setSignUp(!signUp)} className="sign-up-text">
+              Sign up
+            </span>
+          </div>
+        </div>
+      ) : (
+        //dignUp container
+        <div>
+          {" "}
+          <div className="flex-container">
+            <img className="img" src={insta} alt="" />
+            <p>Sign up to see photos and videos from your friends.</p>
+            <form className="flex-form" onSubmit={submitHandler}>
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                className="form"
+                type="text"
+                placeholder="Username "
+              />
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                className="form"
+                type="text"
+                placeholder="Email"
+              />
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className="form"
+                type="password"
+                placeholder="Password"
+              />
+              <button className="button" type="submit">
+                Register
+              </button>
+            </form>
+            {duplicateUser ? ("" ) : ( <div className="dub-message">Login already exists. </div> )}
 
+            {duplicateEmail ? (
+              ""
+            ) : (
+              <div className="dub-message">Email already exists. </div>
+            )}
+            {emailUsernameCorrect ? <div>User created</div> : ""}
+          </div>
+          <div className="sign-up">
+            <p className="no-acc">Have an account? </p>
+            <span onClick={() => setSignUp(!signUp)} className="sign-up-text">
+              Log in
+            </span>
+          </div>
+        </div>
+      )}
+       <Outlet />
     </>
   );
 };
-
-
-
-
-
-
